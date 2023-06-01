a3bb88a7f44097ff06691926f8cde63007849014
nlevin6 <114117629+nlevin6@users.noreply.github.com>
2023-06-01 19:14:01 -0400

    Delete iFINANCE(archived) directory

--- iFINANCE(archived)/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module se2203b.assignments.ifinance {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens se2203b.assignments.ifinance to javafx.fxml;
-    exports se2203b.assignments.ifinance;
-}
